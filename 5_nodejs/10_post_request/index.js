8096ce96dab8e88a68a06efaafc86141ed652c1e
alessandro tofani <alessandro@debian.alessandro>
2020-11-02 16:14:05 +0100

    up

--- 5_nodejs/10_post_request/index.js
+++ 5_nodejs/10_post_request/index.js
@@ -4,6 +4,12 @@ const express = require('express')
 const app = express();
 // permette di usare path
 const path = require('path')
+// uso uuid per assegnare gli id ai commenti
+const {v4: uuid} = require('uuid')
+// lo uso per fare l'override del commento
+const methodOverride = require('method-override')
+
+
 
 // codifica i dati 
 // app.use permette di usarlo per tutte le richieste
@@ -14,19 +20,24 @@ app.use(express.json())
 // setto il path della cartella view e uso i file ejs
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+// uso l'override
+app.use(methodOverride('_method'))
 
 
 // creo la struttura dati con cui andrò ad interagire
 const comments = [
     {
+        id: uuid(),
         username: 'todd',
         comment: ' hello i am todd',
     },    
     {
+        id: uuid(),
         username: 'alex',
         comment: ' hello i am alex',
     },    
     {
+        id: uuid(),
         username: 'bob',
         comment: ' hello i am bob',
     }
@@ -50,12 +61,49 @@ app.post('/comments', (req, res) =>{
     //estraggo le info che dovrò inserire nel database
     const {username, comment} = req.body
     // pusho nel database
-    comments.push({username, comment})
+    comments.push({username, comment, id: uuid()})
     // res.send("it worked!")
     // reindirizzo l'utente nuovamente nella home 
     // se non lo faccio posso avere il problema di risubmission del form
     res.redirect('/comments')
 })
+
+// routing per il singolo commento tramite id
+app.get('/comments/:id', (req, res) =>{
+    // prendo l'id dalla richiesta che fa l'user
+    const {id} = req.params
+    // cerco il commento tra quelli presenti nel database
+    const comment = comments.find( c => c.id === id)
+    // restituisco la pagina con il singolo commento
+    res.render('comments/show', {comment})
+})
+
+// routing per permettere di editare il commento
+app.get('/comments/:id/edit', (req,res) => {
+    // prendo l'id dalla richiesta che fa l'user
+    const {id} = req.params
+    // seleziono il commento con quell'id
+    const comment = comments.find( c => c.id === id)
+    // mando la pagina per poter editare il commento
+    res.render('comments/edit', {comment})
+})
+
+// PATCH request: la uso per fare l'update del contenuto del commento
+// il form non può mandare una richiesta di patch, perciò devo appoggiarmi al metodo precedente
+app.patch('/comments/:id', (req, res) =>{
+    // prendo l'id dalla richiesta che fa l'user
+    const {id} = req.params
+    // seleziono il testo del nuovo commento
+    const newCommentText = req.body.comment
+    // cerco il commento tra quelli presenti nel database
+    const foundComment = comments.find( c => c.id === id)   
+    // cambio il testo del commento con il nuovo commento
+    foundComment.comment = newCommentText
+    // reindirizzo alla home (prende di default il file index)
+    res.render('/comments')
+})
+
+
 
 app.get('/tacos', (req, res) =>{
     res.send('GET /tacos response')
